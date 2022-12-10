4706ff9e46690f7125a8bed498c0848ee8e788e8
Lam Pham <Lam.Pham@thetrainline.com>
2022-12-10 15:46:58 +0100

    Day 10 shorten syntax

--- src/main/kotlin/day10/Day10.kt
+++ src/main/kotlin/day10/Day10.kt
@@ -4,40 +4,32 @@ import utils.println
 
 fun main() {
     println(part1())
-    println(part2())
+    part2()
 }
 
 private fun part1(): Int = with(getSignalMap()) {
-    return getValue(20 - 1) * 20 +
-            getValue(60 - 1) * 60 +
-            getValue(100 - 1) * 100 +
-            getValue(140 - 1) * 140 +
-            getValue(180 - 1) * 180 +
-            getValue(220 - 1) * 220
+    get(20 - 1) * 20 + get(60 - 1) * 60 + get(100 - 1) * 100 + get(140 - 1) * 140 + get(180 - 1) * 180 + get(220 - 1) * 220
 }
 
-private fun part2(): Int {
+private fun part2(){
     val signalMap = getSignalMap()
     val crt = MutableList(6) { MutableList(40) { "." } }
     (0 until 40 * 6).forEach { index ->
         val (column, row) = index / 40 to index % 40
-        val spiritPosition = signalMap.getValue(index) -1
+        val spiritPosition = signalMap[index] -1
         if (row >= spiritPosition && row <= spiritPosition + 2 ) {
             crt[column][row] = "#"
         }
     }
-    crt.forEach {
-        it.joinToString(separator = "").println()
-    }
-    return 0
+    crt.forEach { it.joinToString(separator = "").println() }
 }
 
-private fun getSignalMap(): Map<Int, Int> {
+private fun getSignalMap(): List<Int> {
     var sum = 1
-    return input.flatMap {
+    return listOf(1) + input.flatMap {
         "addx (-*\\d+)".toRegex().find(it)?.groupValues?.let { (_, value) -> listOf(0, value.toInt()) } ?: listOf(0)
-    }.mapIndexed { index, value ->
+    }.map { value ->
         sum += value
-        (index + 1) to sum
-    }.toMap() + mapOf(0 to 1)
+        sum
+    }
 }
